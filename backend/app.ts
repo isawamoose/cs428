004f195af120542376c9264454479c88126c7699
John <johnnephiolson@gmail.com>
2025-02-22 14:22:20 -0700

    Fixed minor errors in import statements

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { login } from './controllers/loginController';
+import { login } from './userController';
 
 const app = express();
 
--- backend/userController.ts
+++ backend/userController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import { UserService } from 'userService';
+import { UserService } from './userService';
 
 const userService = new UserService();
 
 export async function login(req: Request, res: Response) {
     const { username, password } = req.body;
 
-    if (await userService.validateUser(username, password)) {
+    if (await userService.validateExistingUser(username, password)) {
         res.status(200).send('You have logged in');
     } else {
         res.status(401).send('Invalid credentials');
