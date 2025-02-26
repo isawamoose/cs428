0d392ccf537925d4248dfdd15d1e519edb961d39
Tess-H <122323641+Tess-H@users.noreply.github.com>
2025-02-26 12:16:33 -0700

    Updated Login to support FakeData and login service
    What I did:
    1.	Added FakeData class in front end shared folder
    a.	Contains a get for authotekn generated in Authoken.ts
    b.	Contains a list of Profiles
    c.	Contains a function to get the first Proflile in the list to use for login
    2.	Added Authtoken class in front end shared folder
    a.	Generates an authoken either via uuid or random string generation
    3.	Added LoginService class in services folder in src of front end
    a.	Contains the function login that grabs the first Profile from FakeData and returns it.
    4.	Added UserInfoProvider class in components folder in src of front end
    a.	Class used to globally and locally store user information to be used across the different pages/views after logging in or registering.
    5.	Added < UserInfoProvider > to App.tsx
    6.	Updated Login.tsx to call login service and update user info in UserInfoProvider
    7.	Updated Login.css to fit the prototype

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,19 @@
+import { UserInfoContext } from "../components/UserInfoProvider";
+import { useContext, useEffect } from "react";
+
 const Home = () => {
+  const { currentUser, authToken } = useContext(UserInfoContext);
+
+  //THIS WAS JUST FOR TESTING IF PROFILE INFORMATION WAS BEING STORED CORRECTLY AFTER LOGGING IN, FEEL FREE TO DELETE
+  useEffect(() => {
+    // Log the user info (even if not logged in)
+    if (currentUser) {
+      console.log("Stored User Info:", currentUser.shortProfile);
+    } else {
+      console.log("No user logged in");
+    }
+  }, [currentUser, authToken]); // Re-run when currentUser or authToken changes
+
   return (
     <div>
       <h2>Home Page</h2>
