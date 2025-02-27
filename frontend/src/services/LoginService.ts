b3caa9aecc44995a04da8c81fc8283f8cbb60f16
Stephen Amos <amos.stephen.m@gmail.com>
2025-02-27 10:39:36 -0700

    Use const instead of let on constants, add uuid deps to package.json

--- frontend/src/services/LoginService.ts
+++ frontend/src/services/LoginService.ts
@@ -1,17 +1,15 @@
-import { FakeData } from "../../shared/util/FakeData";
-import { Profile } from "../../shared/Profile";
-import { AuthToken } from "../../shared/util/AuthToken";
+import { FakeData } from '../../shared/util/FakeData';
+import { Profile } from '../../shared/Profile';
+import { AuthToken } from '../../shared/util/AuthToken';
 
 export class LoginService {
-  public async login(
-    username: string,
-    password: string
-  ): Promise<[Profile, AuthToken]> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async login(username: string, password: string): Promise<[Profile, AuthToken]> {
     // TODO: Replace with the result of calling the server
-    let userProfile = FakeData.instance.firstUser; // for now get the first of the fake users to use for login
+    const userProfile = FakeData.instance.firstUser; // for now get the first of the fake users to use for login
 
     if (userProfile === null) {
-      throw new Error("Invalid alias or password");
+      throw new Error('Invalid alias or password');
     }
 
     return [userProfile, FakeData.instance.authToken];
