e336169064abceacc88ce8a103e067de132d8c5e
Beau Yancey <yancey.beau.j@gmail.com>
2025-03-10 20:14:41 -0600

    added page highlighting to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,16 @@
 import { LuHouse, LuBone, LuUser } from 'react-icons/lu'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
 
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
 
 	return (
 		<nav className='app-navbar'>
-			<LuBone onClick={() => navigate('/app/matches')}/>
-			<LuHouse onClick={() => navigate('/app')}/>
-			<LuUser onClick={() => navigate('/app/profile')}/>
+			<LuBone color={pathname.includes('/app/matches') ? 'white' : '#81D3FF' } onClick={() => navigate('/app/matches')}/>
+			<LuHouse color={pathname === '/app' ? 'white' : '#81D3FF' } onClick={() => navigate('/app')}/>
+			<LuUser color={pathname.includes('/app/settings') ? 'white' : '#81D3FF' } onClick={() => navigate('/app/settings')}/>
 		</nav>
 	)
 }
