141c731b0a2306152ca8a4e3abc41895e55dea30
Beau Yancey <yancey.beau.j@gmail.com>
2025-03-11 23:53:54 -0600

    add logout function

--- frontend/src/pages/ProfileSettings.tsx
+++ frontend/src/pages/ProfileSettings.tsx
@@ -1,13 +1,29 @@
 import { Profile } from "@shared/Profile";
 import './ProfileSettings.css'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LuChevronRight } from "react-icons/lu";
+import { useState } from "react";
+import { LoginService } from "../services/LoginService";
 
 interface Props {
 	user: Profile | null
 }
 
 const ProfileSettings = (props: Props) => {
+
+	const [service] = useState(new LoginService());
+	const navigate = useNavigate();
+
+	const logout = async () => {
+		await service.logout()
+		navigate('/')
+	}
+
+	const deleteAccount = async () => {
+		await service.deleteAccount()
+		navigate('/')
+	}
+
 	return (
 		<div className='settings-page'>
 			<div className='settings-header'>
@@ -26,8 +42,8 @@ const ProfileSettings = (props: Props) => {
 			</div>
 
 			<div className='settings-buttons'>
-				<button className='btn-yellow'>Log Out</button>
-				<button className='btn-red'>Delete Account</button>
+				<button className='btn-yellow' onClick={logout}>Log Out</button>
+				<button className='btn-red' onClick={deleteAccount}>Delete Account</button>
 			</div>
 		</div>
 	);
